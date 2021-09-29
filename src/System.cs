0f7548df480d35f631ef0a1117d604261c36ea40
Aaron Winter <winter.aaron93@gmail.com>
2021-09-29 18:47:53 +0200

    move build-in system to system.cs

--- src/System.cs
+++ src/System.cs
@@ -12,4 +12,18 @@ namespace Bitron.Ecs
     {
         void Run(EcsWorld world);
     }
+
+    public class RemoveAllComponentsOfType<T> : IEcsSystem where T : struct
+    {
+        public void Run(EcsWorld world)
+        {
+            var query = world.Query<T>().End();
+            var pool = query.GetPool<T>();
+
+            foreach (var entity in query)
+            {
+                pool.Remove(entity);
+            }
+        }
+    }
 }
